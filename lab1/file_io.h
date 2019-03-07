5d9cf10111598034db0552a5f2a9a2ed254892fd
cyj407 <dfghj345678@gmail.com>
2019-03-07 11:26:49 +0800

    Add file_io.cpp

---
+++ lab1/file_io.h
@@ -0,0 +1,17 @@
+#include<cstdio>
+#include<iostream>
+
+struct Processor {
+	int id;
+	int ability;
+};
+
+struct Task {
+	int id;
+	int release_t;
+	int exec_t;
+	int deadline;
+	int period;
+	int preemption;
+	int type;
+};
